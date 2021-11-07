b86d4ab5b5aba2a915f10c311433df633fb6ed63
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2021-11-07 20:18:20 +0530

    Add files via upload

--- Polygon-Area-Calculator/shape_calculator.py
+++ Polygon-Area-Calculator/shape_calculator.py
@@ -1,7 +1,60 @@
 class Rectangle:
-    pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def __repr__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
 
+    def set_width(self, width):
+        self.width = width
 
-class Square:
-    pass
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        else:
+            return ("*" * self.width + "\n") * (self.height)
+
+    def get_amount_inside(self, shape):
+        if type(shape) == Square:
+            return (self.width // shape.side) * (self.height // shape.side)
+        elif type(shape) == Rectangle:
+            return max((self.width // shape.width) * (self.height // shape.height), (self.width // shape.height) * (self.height // shape.width))
+
+class Square(Rectangle):
+    def __init__(self, side):
+        self.side = side
+        self.width = side
+        self.height = side
+
+    def __repr__(self):
+        return f"Square(side={self.side})"
+
+    def set_side(self, side):
+        self.side = side
+        self.width = side
+        self.height = side
+
+    def set_width(self, width):
+        self.set_side(width)
+
+    def set_height(self, height):
+        self.set_side(height)
+
+    def get_amount_inside(self, shape):
+        if type(shape) == Square:
+            return (self.side // shape.side) ** 2
+        elif type(shape) == Rectangle:
+            return (self.side // shape.width) * (self.side // shape.height)
